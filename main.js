ba079f902aa8f9ec283ff5166a36c6928fefac6c
Ricieri Rostirolla <114628700+rrostirolla@users.noreply.github.com>
2022-11-17 16:00:09 -0300

    fix: correção inputs and new feat
    Feat: novo comparador de igualdade adicionado.

--- main.js
+++ main.js
@@ -7,19 +7,27 @@ form.addEventListener('submit', function(e) {
     const numeroA = parseInt(document.getElementById('number-a').value)
     const mensagemDeSucesso = `O valor <b>B (${numeroB}) <u>é maior</u></b>  que o valor <b>A (${numeroA})</b>`
     const mensagemDeErro = `O valor <b>B (${numeroB}) <u>não</u></b> é maior que o valor <b>A (${numeroA})</b>`
+    const mensagemDeIgualdade = `O valor <b>B (${numeroB}) é <u>igual</u></b> o valor <b>A (${numeroA})</b>`
+    const containerMensagemErro = document.querySelector('.error-message');
+    const containerMensagemSucesso = document.querySelector('.success-message');
+    const containerMensagemIgualdade = document.querySelector('.equal-message');
     
+    containerMensagemSucesso.style.display = 'none';
+    containerMensagemErro.style.display = 'none';
+    containerMensagemIgualdade.style.display = 'none';
+
     if (numeroA < numeroB) {
-        const containerMensagemSucesso = document.querySelector('.success-message');
         containerMensagemSucesso.innerHTML = mensagemDeSucesso
         containerMensagemSucesso.style.display = 'block'
-
+    } else if (numeroA == numeroB){
+        containerMensagemIgualdade.innerHTML = mensagemDeIgualdade
+        containerMensagemIgualdade.style.display = 'block'
     } else {
-        const containerMensagemErro = document.querySelector('.error-message');
         containerMensagemErro.innerHTML = mensagemDeErro
         containerMensagemErro.style.display = 'block' 
     }
 
-    numeroA = ''
-    numeroB = ''
+    document.getElementById('number-a').value = ''
+    document.getElementById('number-b').value = ''
 });
 
