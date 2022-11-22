1498bd760e5efe16d0372701317938c3e7f9fb16
Ricieri Rostirolla <114628700+rrostirolla@users.noreply.github.com>
2022-11-22 09:31:29 -0300

    Fix: required and line-through

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-    $('button').click(function(e) {
+    $('form').submit(function(e) {
         e.preventDefault();
         const enderecoNovoItem = $('#nova-tarefa').val();
         const novoItem = $(`<li class="novo-item">${enderecoNovoItem}</li>`);
         $(novoItem).appendTo('ol');
         $(novoItem).fadeIn(1000);
         $('#nova-tarefa').val('');
+    });
+
+    $('ol').on('click', 'li', function (e) {
+        $(this).css('text-decoration', 'line-through');
     });
 });
