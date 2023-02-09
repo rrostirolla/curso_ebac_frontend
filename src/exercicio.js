133b12ca3b03d97a5bcadb2066c718270d2469fa
Ricieri Rostirolla <114628700+rrostirolla@users.noreply.github.com>
2023-02-09 08:14:44 -0300

    initial commit

---
+++ src/exercicio.js
@@ -0,0 +1,12 @@
+"use strict";
+function area(numero1, numero2) {
+    return numero1 * numero2;
+}
+;
+function dizOla(nome) {
+    return `Ola ${nome}`;
+}
+;
+//arrow function
+const areaArrow = (numero1, numero2) => numero1 * numero2;
+const dizOlaArrow = (nome) => `Olá ${nome}`;
---
+++ src/exercicio.ts
@@ -0,0 +1,12 @@
+function area(numero1: number, numero2: number) : number {
+    return numero1 * numero2
+};
+
+function dizOla(nome: string) : string {
+    return `Ola ${nome}`
+};
+
+//arrow function
+const areaArrow = (numero1:number, numero2:number) : number => numero1 * numero2;
+
+const dizOlaArrow = (nome:string) : string => `Olá ${nome}`;
